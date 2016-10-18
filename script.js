e535af4a5e9606bf69f245439b8e99a3b5f1c81f
elisa lee <elisa.my.lee@gmail.com>
2016-10-18 13:06:55 -0500

    use Angular instead of jquery

--- script.js
+++ script.js
@@ -1,58 +1,22 @@
-$(document).ready(function() {
-  var totalSalary = 0;
-  var salaries = [];
+var app = angular.module('empApp', []);
 
-// removes Employee entry
-  $('#remove').on('click', '.delete', function(event){
-    var x = ($(event.target).index()) - 1;
-    $(this).closest('button').remove();
-    console.log(x);
+app.controller('employeeController', function () {
+  var self = this;
 
-    // updates monthly salary expenditure by deleting salary of removed employee
-    var deletedSalary = parseFloat((salaries[x]));
-    console.log(deletedSalary);
-    totalSalary = totalSalary - deletedSalary;
-    $('#expenditure').find('p').text(totalSalary);
+  self.totalSalary = 0;
+  self.salaries = [];
+  self.employees = [];
 
-    //removes employee information
-    for(var i=0; i<5; i++){
-      $('#empList li').eq(i).find('.person').eq(x).remove();
-    };
-  });
-
-//submit employee information on submit
-  $('#empInfo').on('submit', function(event){
-    event.preventDefault();
-    var employee = {};
-    var entry = $('#empInfo').serializeArray();
-
-    entry.forEach(function(element, index) {
-      employee[element.name] = element.value;
-    //clearing the input form on submit
-      $('#empInfo').find('input[type=text]').val('');
-      $('#empInfo').find('input[type=number]').val('');
-    });
-    var emp = [employee['firstName'], employee['lastName'], employee['idNumber'], employee['jobTitle'], employee['salary']];
-
-    addEmployee(emp);
-    addSalary(emp);
-    addDelete();
-  });
-
-function addEmployee(person) {
-  for(var i=0; i<5; i++){
-    $('#empList li').eq(i).append('<br><p class="person">' + person[i] + '<p>');
+  self.createEmployee = function () {
+    var salary = parseInt(self.person.salary)/12;
+    self.employees.push(angular.copy(self.person));
+    self.salaries.push(angular.copy(salary));
+    self.totalSalary += salary;
   };
-}
 
-function addSalary(person) {
-  var salary = parseFloat(person[4]/12);
-  salaries.push(salary);
-  totalSalary = parseFloat(salary + totalSalary);
-  $('#expenditure').find('p').text(totalSalary.toFixed(2));
-}
-
-function addDelete() {
-  $('#remove').append('<button class="delete">X</button>');
-  }
+  self.deleteEmployee = function ($index, employee) {
+    self.employees.splice($index, 1);
+    var deletedValue = self.salaries.splice($index, 1);
+    self.totalSalary -= deletedValue;
+  };
 });
